8a470779e3695c6e3c4d4b4194925d4af8adda7e
Agata Fernanda <agata.fernanda4@gmail.com>
2023-10-17 11:29:17 -0300

    Camada de xadrez e imprimindo tabuleiro

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -2,11 +2,13 @@ package aplicacao;
 
 import tabuleiro.Posicao;
 import tabuleiro.Tabuleiro;
+import xadrez.PartidaXadrez;
 
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		Tabuleiro tabuleiro = new Tabuleiro(8,8);
+		PartidaXadrez partida = new PartidaXadrez();
+		UI.imprimeTabuleiro(partida.getPecas());
 	}
 }
